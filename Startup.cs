d97c39784ec890d8e25b3bbff512c6d209bd9e3c
David Chen Billdal <davidbilldal@gmail.com>
2019-12-29 22:52:13 +0100

    Add ToDo list example to application

--- Startup.cs
+++ Startup.cs
@@ -5,9 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProjectDemoPage.Services;
 
 namespace ProjectDemoPage
 {
@@ -23,8 +26,29 @@ namespace ProjectDemoPage
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<ICosmosDbService>(
+                InitializeCosmosClientInstanceAsync(Configuration.GetSection("CosmosDb"))
+                .GetAwaiter().GetResult());
             services.AddControllersWithViews();
             services.AddHttpClient();
+        }
+
+        private static async Task<CosmosDbService> InitializeCosmosClientInstanceAsync(
+            IConfigurationSection configurationSection)
+        {
+            string databaseName = configurationSection.GetSection("DatabaseName").Value;
+            string containerName = configurationSection.GetSection("ContainerName").Value;
+            string account = configurationSection.GetSection("Account").Value;
+            string key = configurationSection.GetSection("Key").Value;
+            CosmosClientBuilder clientBuilder = new CosmosClientBuilder(account, key);
+            CosmosClient client = clientBuilder
+                                .WithConnectionModeDirect()
+                                .Build();
+            CosmosDbService cosmosDbService = new CosmosDbService(client, databaseName, containerName);
+            DatabaseResponse database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+            await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
+
+            return cosmosDbService;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
