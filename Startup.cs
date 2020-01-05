e88b652ab3075e3de6f4bbd729b1d318338d5c9b
ntnudavidcb <davidgchengbilldal@gmail.com>
2020-01-05 15:55:50 +0100

    Fix small bug in startup class

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace ProjectDemoPage
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public object ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<ICosmosDbService>(
                 InitializeCosmosClientInstanceAsync(Configuration.GetSection("CosmosDb"))
@@ -33,7 +33,6 @@ namespace ProjectDemoPage
             services.AddSingleton<IAzureBlobService, AzureBlobService>();
             services.AddControllersWithViews();
             services.AddHttpClient();
-            return services;
         }
 
         private static async Task<CosmosDbService> InitializeCosmosClientInstanceAsync(
