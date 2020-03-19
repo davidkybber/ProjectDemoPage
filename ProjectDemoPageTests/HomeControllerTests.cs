e878733c816b0d1bada5afe6ee79b1783def5801
David Chen Billdal <davidbilldal@gmail.com>
2020-03-19 16:07:20 +0100

    Add tests for home controller

--- ProjectDemoPageTests/HomeControllerTests.cs
+++ ProjectDemoPageTests/HomeControllerTests.cs
@@ -1,14 +1,40 @@
 using System;
 using Xunit;
+using ProjectDemoPage.Controllers;
+using Moq;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ProjectDemoPageTests
 {
-    public class UnitTest1
+    public class HomeControllerTests
     {
         [Fact]
-        public void Test1()
+        public void Index_Default_ReturnsViewResult()
         {
+            // Arrange
+            var stubLogger = new Mock<ILogger<HomeController>>();
+            var homeController = new HomeController(stubLogger.Object);
 
+            // Act
+            var result = homeController.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Privacy_Default_ReturnsViewResult()
+        {
+            // Arrange
+            var stubLogger = new Mock<ILogger<HomeController>>();
+            var homeController = new HomeController(stubLogger.Object);
+
+            // Act
+            var result = homeController.Privacy();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
         }
     }
 }
